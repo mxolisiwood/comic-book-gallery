f08b11a18c523d5e2996e6a2b53f716ee9182ff3
Mxolisi Silabela <silabelamxolisi@gmail.com>
2019-10-31 12:21:36 +0100

    uodated comic book detail view

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,13 +10,27 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-        //    if (DateTime.Today.DayOfWeek== DayOfWeek.Wednesday)
-        //    {
-        //        return Redirect("/");
-        //    }
 
-        //    return Content("I Love Tania");
-        return View();
+            ViewBag.SeriesTitle = "The Amazing Spider-man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus'" +
+                               " life and his one, last, great act of revenge! Even if Spider-Man survives... " +
+                               "<strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+            //    if (DateTime.Today.DayOfWeek== DayOfWeek.Wednesday)
+            //    {
+            //        return Redirect("/");
+            //    }
+
+            //    return Content("I Love Tania");
+            return View();
 
         }
     }
